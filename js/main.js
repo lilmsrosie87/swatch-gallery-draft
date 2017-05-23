f73ebd3e562cee3cc5458d119bb2bd8cd7d5a0e0
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-05-22 23:26:57 -0700

    cleaned up getSwatch function

--- js/main.js
+++ js/main.js
@@ -2,14 +2,15 @@ function getThatSwatch () {
 	document.write('Swatches for deeper complexions! Coming Soon');
 }
 
-function getSwatch (image, color, brand,formula) {
-	var swatchSearch = image + color;
-	var swatchClick = image + brand + formula;
-var swatchInfo = [swatchSearch, swatchClick];
+function getSwatch(image, color, brand, formula, price) {
+	var swatchSearch = 'image' + 'color';
+	var swatchClick = 'price' + 'brand' + 'formula';
+var swatchInfo = [swatchSearch + swatchClick];
 	return swatchInfo;
 }
 
-var swatchOne
+var swatchOne = getSwatch(1,'red','MAC','matte',16)
+
 
 
 
