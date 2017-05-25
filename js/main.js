7fc099838a9fce859c90ee44bea48d16bf9862d8
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-05-24 21:45:28 -0700

    added qoutes to getSwatch function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function getThatSwatch () {
 	document.write('Swatches for deeper complexions! Coming Soon');
 }
 
-function getSwatch(image, color, brand, formula, price) {
+function getSwatch('image', 'color', 'brand', 'formula', 'price') {
 	var swatchSearch = 'image' + 'color';
 	var swatchClick = 'price' + 'brand' + 'formula';
 var swatchInfo = [swatchSearch + swatchClick];
@@ -10,7 +10,7 @@ var swatchInfo = [swatchSearch + swatchClick];
 }
 
 var swatchOne = getSwatch(1,'red','MAC','matte',16)
-
+document.write(swatchOne)
 
 
 
