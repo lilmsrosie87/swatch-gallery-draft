e06e772a8ea2f06636cf995cc64dcc157c855951
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-06-18 22:21:02 -0700

    changed constructor notation

--- js/main.js
+++ js/main.js
@@ -10,18 +10,31 @@ function getThatSwatch () {
 //}
 
 
-//constructor notation
+//constructor notation with many objects
+function swatch (image, name, hue, brand, price) {
+	this.image = 
+	this.name = name;
+	this.hue = hue;
+	this.brand = brand;
+	this.price = price;
+	
+	this.findSwatch = function() {
+		return this.image + this.name; 
+};
+	
+
 var lipstick = new Object();
 lipstick.image = 'img/purple_lip.jpg';
 lipstick.name = 'Shameless';
 lipstick.hue = 'purple';
 lipstick.brand = 'Revlon';
 
-lipstick.searchSwatch = function() {
+lipstick.search = function() {
 	return this.image + this.name;
 
 };
-
+//var getSwatch = document.getElementById('lipstickImage');
+//getSwatch.textContent = lipstick.image;
 	
 //var findSwatch = document.getElementById('hue');
 //findSwatch.textContent = swatchOne.hue;
