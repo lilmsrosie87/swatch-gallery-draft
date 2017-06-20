99a69b59cac1eac572a0ba5b26b4e90354302973
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-06-19 21:24:46 -0700

    added more products with construction notation

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ function getThatSwatch () {
 
 
 //constructor notation with many objects
-function swatch (image, name, hue, brand, price) {
-	this.image = 
+function Product (image, name, hue, brand, price) {
+	this.image = image
 	this.name = name;
 	this.hue = hue;
 	this.brand = brand;
@@ -22,12 +22,17 @@ function swatch (image, name, hue, brand, price) {
 		return this.image + this.name; 
 };
 	
+	
+//updating an object page 107	
+var teddyBare = new product('img/nude_lip.jpg', 'Teddy Bare', 'brown', 'Milani', 8);
+var shameLess = new product('img/purple_lip.jpg', 'Shameless', 'purple', 'Revlon', 7);
 
-var lipstick = new Object();
-lipstick.image = 'img/purple_lip.jpg';
-lipstick.name = 'Shameless';
-lipstick.hue = 'purple';
-lipstick.brand = 'Revlon';
+
+//var lipstick = new product();
+//lipstick.image = 'img/purple_lip.jpg';
+//lipstick.name = 'Shameless';
+//lipstick.hue = 'purple';
+//lipstick.brand = 'Revlon';
 
 lipstick.search = function() {
 	return this.image + this.name;
