90942da3f26f1ab38836b7c201c2f7d6503421da
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-08-08 23:02:26 -0700

    altered variable and updated array

--- js/main.js
+++ js/main.js
@@ -11,39 +11,39 @@ function getThatSwatch () {
 
 
 //constructor notation with many objects
-function product (image, name, hue, brand, price) {
-	this.image = image
-	this.name = name;
-	this.hue = hue;
-	this.brand = brand;
-	this.price = price;
+// function product (image, name, hue, brand, price) {
+	// this.image = image
+	// this.name = name;
+	// this.hue = hue;
+	// this.brand = brand;
+	// this.price = price
+	
+var lipstick = new product(); {
+	lipstick.image = 'img/purple_lip.jpg';
+	lipstick.name = 'Shameless';
+	lipstick.hue = 'purple';
+	lipstick.brand = 'Revlon';
+
+//lipstick.search = function() {
+	//return this.image + this.name
 
 //from https://stackoverflow.com/questions/14299917/making-a-simple-javascript-image-gallery
 
 	var ImageCnt = 0
 	
-	this.findSwatch = function() {
-		return this.image + this.name; 
+	//this.findSwatch = function() {
+		//return this.image + this.name; 
 		
-	function product(){
+	function swatch(){
     ImageCnt++;
-    document.getElementById("whiteBox").style.background = 'url(' + product[ImageCnt] + ')';
+    document.getElementById('gallery').style.background = 'lipstick(' + [ImageCnt] + ')';
 };
 	
 	
 //updating an object page 107	
-//var teddyBare = new product('img/nude_lip.jpg', 'Teddy Bare', 'brown', 'Milani', 8);
-//var shameLess = new product('img/purple_lip.jpg', 'Shameless', 'purple', 'Revlon', 7);
+var teddyBare = new product('img/nude_lip.jpg', 'Teddy Bare', 'brown', 'Milani', 8);
+var shameLess = new product('img/purple_lip.jpg', 'Shameless', 'purple', 'Revlon', 7);
  
-
-//var lipstick = new product();
-//lipstick.image = 'img/purple_lip.jpg';
-//lipstick.name = 'Shameless';
-//lipstick.hue = 'purple';
-//lipstick.brand = 'Revlon';
-
-//lipstick.search = function() {
-	//return this.image + this.name;
 //};
 
 //var getSwatch = document.getElementById('lipstickImage');
