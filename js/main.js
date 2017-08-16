77096963bf2fede6c0f3afd2b35fbb9954969562
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-08-15 22:38:07 -0700

    alted js code for only constructor notation and adding new objects

--- js/main.js
+++ js/main.js
@@ -5,22 +5,21 @@ function getThatSwatch () {
 
 
 //constructor notation with many objects
-// function product (image, name, hue, brand, price) {
-	// this.image = image
-	// this.name = name;
-	// this.hue = hue;
-	// this.brand = brand;
-	// this.price = price
-	
+ function product(image, name, hue, brand, price) {
+	 this.image = image
+	 this.name = name;
+	 this.hue = hue;
+	 this.brand = brand;
+	 this.price = price;
+}
+	 
 var lipstick = new product(); {
 	lipstick.image = 'img/purple_lip.jpg';
 	lipstick.name = 'Shameless';
 	lipstick.hue = 'purple';
 	lipstick.brand = 'Revlon';
 
-//lipstick.search = function() {
-	//return this.image + this.name
-
+}
 //from https://stackoverflow.com/questions/14299917/making-a-simple-javascript-image-gallery
 
 	var ImageCnt = 0
@@ -36,21 +35,6 @@ var lipstick = new product(); {
 	
 //updating an object page 107	
 var teddyBare = new product('img/nude_lip.jpg', 'Teddy Bare', 'brown', 'Milani', 8);
-var shameLess = new product('img/purple_lip.jpg', 'Shameless', 'purple', 'Revlon', 7);
- 
-//};
+var shameLess = new product('img/purple_lip.jpg', 'Shameless', 'purple', 'Revlon', 7);]
 
-//var getSwatch = document.getElementById('lipstickImage');
-//getSwatch.textContent = lipstick.image;
-	
-//var findSwatch = document.getElementById('hue');
-//findSwatch.textContent = swatchOne.hue;
-
-
-//var swatchTwo = getSwatch(2,'pink','Petal Pink','YSL','glossy',34)
- 
-
-
-
-
- 
+	document.write(teddyBare);
