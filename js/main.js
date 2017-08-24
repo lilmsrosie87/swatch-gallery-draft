45702c33eb18aadd60da2703fb9c849857b1de63
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-08-23 22:29:25 -0700

    working with link on line 5

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ function getThatSwatch () {
 	document.write('Swatches for deeper complexions! Coming Soon');
 }
 
+// http://w3schools.invisionzone.com/topic/190-display-images-with-javascript/
+checkx=document.images[0].srcif(x="img/nude_lip.jpg"){
+	alert("The image is displayed!")
+	}else{
+	alert("The image isn't here!")
+}
 
 
 //constructor notation with many objects
