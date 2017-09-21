4c61fdc9f6ce76a1a92dda8ca87255d072efd1cc
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-09-20 23:06:30 -0700

    adding cache for search

--- js/filter-search.js
+++ js/filter-search.js
@@ -3,4 +3,20 @@
 	var $search = $('#filter-search');
 	var cache = [];
 	
+	$imgs.each(function() {
+		cache.push({
+			element: this,
+			text: this.alt.trim().toLowerCase()
+		});
+	});
 	
+function filter() {
+	var query = this.value.trim().toLowerCase();
+	
+	cache.forEach(function(img) {
+		var index = 0;
+		if (query) {
+			index = img.text.indexOf(query);
+		}
+		
+		
