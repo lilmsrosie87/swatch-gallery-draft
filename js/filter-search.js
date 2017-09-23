84c9d3515f07ff568f06772c4fb192d47cc45a9e
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-09-22 22:31:07 -0700

    made search IIFE in filter-search as note. Starting different search code

--- js/filter-search.js
+++ js/filter-search.js
@@ -1,31 +1,35 @@
-(function() {
-	var $imgs = $('#gallery img');
-	var $search = $('#filter-search');
-	var cache = [];
+
+
+// from JS book, but console stated that line 2 and 31 are undefined
+
+// (function() {
+	// var imgs = ('#gallery img');
+	// var search = ('#filter-search');
+	// var cache = [];
 	
-	$imgs.each(function() {
-		cache.push({
-			element: this,
-			text: this.alt.trim().toLowerCase()
-		});
-	});
+	// imgs.each(function() {
+		// cache.push({
+			// element: this,
+			// text: this.alt.trim().toLowerCase()
+		// });
+	// });
 	
-	function filter() {
-		var query = this.value.trim().toLowerCase();
+	// function filter() {
+		// var query = this.value.trim().toLowerCase();
 	
-		cache.forEach(function(img) {
-			var index = 0;
-			if (query) {
-				index = img.text.indexOf(query);
-			}
+		// cache.forEach(function(img) {
+			// var index = 0;
+			// if (query) {
+				// index = img.text.indexOf(query);
+			// }
 		
-			img.element.style.display = index === -1 ? 'none' : ''; 
-		});
-	}
+			// img.element.style.display = index === -1 ? 'none' : ''; 
+		// });
+	// }
 	
-	if ('oninput' in $search[0]) {
-		$search.on('input', filter);
-	} else {
-		$search.on('keyup', filter);
-	}
-}());
+	// if ('oninput' in $search[0]) {
+		// $search.on('input', filter);
+	// } else {
+		// $search.on('keyup', filter);
+	// }
+// }());
